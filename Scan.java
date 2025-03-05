b230d1898b9dbab883f8859daa9e93955b91b2e9
willjc-works <willjc003@gmail.com>
2025-03-05 14:57:56 -0500

    Update Scan.java

--- Scan.java
+++ Scan.java
@@ -1,3 +1,22 @@
+
+// Program: Scan
+// Written by: William Cobb
+// Description: Declares a Scanner object to receive user input and then outputs each digit
+// Challenges: For loop that calls Math class function in order to calculate each digit
+// Time Spent: 1:30
+//                   Revision History
+// Date:                   By:               Action:
+// ---------------------------------------------------
+// 01/30/2024             WC             Declared variables to hold user input
+// 01/30/2024             WC             Store user input as string    
+// 01/30/2024             WC             Convert user input to integer  
+// 02/05/2025              WC             Program refinement
+// 02/05/2025              WC             Extract digits in for loop
+
+/* Scan.java */
+
+import java.util.Scanner;
+
 public class Scan 
 {
  
@@ -15,7 +34,7 @@ public class Scan
             System.out.printf(" %d -", digitToPrint);
         }
         
-	      System.out.println();        
+	System.out.println();        
         input.close(); // Close scanner in memory
         System.exit(0);
       
